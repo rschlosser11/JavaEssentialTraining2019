cf8dc82cb0b28779bc5237377e537d03f3659965
rschlosser11 <rosalinschlosser@gmail.com>
2020-12-22 13:59:04 -0600

    declare and use enum

---
+++ src/com/company/model/ClothingSize.java
@@ -0,0 +1,18 @@
+package com.company.model;
+
+public enum ClothingSize {
+    S("Small"), M("Medium"), L("Large");
+
+    private String description;
+
+    private ClothingSize(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+    
+    
+}
