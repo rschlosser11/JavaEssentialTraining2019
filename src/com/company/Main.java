93a771383d828dce9048411ce62834139f40b9c3
rschlosser11 <rosalinschlosser@gmail.com>
2020-12-22 08:33:04 -0600

    convert primitive values to strings

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,21 @@ public class Main {
         int result = 10 + 12;
         String answer = "The answer is " + result;
         System.out.println(answer);
+
+        String howMany = 20 + " things";
+        System.out.println(howMany);
+
+        var result2 = 10 + 20;
+        int intV = 42;
+        var fromInt = Integer.toString(intV);
+        System.out.println(fromInt);
+
+        boolean boolV = true;
+        var fromBool = Boolean.toString(boolV);
+        System.out.println(fromBool);
+
+        long longV = 10_000_000;
+        var fromLong = Long.toString(longV);
+        System.out.println(fromLong);
     }
 }
