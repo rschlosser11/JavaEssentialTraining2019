8126d130574ace8c255067889dc6fbdda35f80e5
rschlosser11 <rosalinschlosser@gmail.com>
2020-12-22 15:24:20 -0600

    manage resizable arrays with lists

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,52 @@
 package com.company;
 
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.company.model.ClothingItem;
 import com.company.model.ClothingSize;
 import com.company.model.Hat;
 import com.company.model.Shirt;
 
-import java.text.NumberFormat;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        var shirt = new Shirt(
-                ClothingSize.L,
-                19.99,
-                3);
-        displayItemDetails(shirt);
+        // var shirt = new Shirt(
+        //         ClothingSize.L,
+        //         19.99,
+        //         3);
+        // displayItemDetails(shirt);
 
-        var hat = new Hat(
-                ClothingSize.M,
-                29.99,
-                1);
-        displayItemDetails(hat);
+        // var hat = new Hat(
+        //         ClothingSize.M,
+        //         29.99,
+        //         1);
+        // displayItemDetails(hat);
+
+        // a list is an interface -> any class that implements this interface is garunteed to have these methods
+        
+        List<String> colors = new ArrayList<>();
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Blue");
+        for (int i = 0; i < colors.size(); i++) {
+            System.out.println(colors.get(i));
+        }
+
+        List<ClothingItem> items = new ArrayList<>();
+        items.add(new Hat(
+            ClothingSize.M,
+            29.99,
+            1));
+        items.add(new Shirt(
+            ClothingSize.L,
+            19.99,
+            3));
+        for (ClothingItem item : items) {
+            displayItemDetails(item);
+        }
     }
 
     private static void displayItemDetails(ClothingItem item) {
